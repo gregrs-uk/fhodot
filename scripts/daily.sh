1b6aed2090b9c67e78a6a2c7e39efcce7474efc3
Greg <gregrs@fastmail.co.uk>
2024-01-10 10:44:31 +0000

    Use Geofabrik UK download not deprecated GB & NI

--- scripts/daily.sh
+++ scripts/daily.sh
@@ -3,8 +3,8 @@ cd /home/gregrs/fhodot || exit 1
 source venv/bin/activate || exit 1
 # overwrites previous file
 wget --no-verbose \
-	http://download.geofabrik.de/europe/britain-and-ireland-latest.osm.pbf \
-	-O import/osm/britain-and-ireland-latest.osm.pbf && \
+	http://download.geofabrik.de/europe/united-kingdom-latest.osm.pbf \
+	-O import/osm/united-kingdom-latest.osm.pbf && \
 	scripts/import_osm.sh
 python -m scripts.update_fhrs
 python -m scripts.calculate_statistics
--- scripts/import_osm_config.sh
+++ scripts/import_osm_config.sh
@@ -2,5 +2,5 @@
 
 imposm_binary_dir=/imposm
 osm_import_dir=/fhodot/import/osm
-data_file=$osm_import_dir/britain-and-ireland-latest.osm.pbf
+data_file=$osm_import_dir/united-kingdom-latest.osm.pbf
 database_name=gregrs_fhodot
