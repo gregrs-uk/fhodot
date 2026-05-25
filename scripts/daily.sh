6f2fffb134fc4b77431c13f662abe89ddccc3ac5
Gregrs <gregrs@fastmail.co.uk>
2026-05-25 18:20:30 +0000

    Check dev server software versions in daily script

--- scripts/daily.sh
+++ scripts/daily.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/gregrs/fhodot || exit 1
 source venv/bin/activate || exit 1
+scripts/software_versions.sh | diff - software_versions.txt
 # overwrites previous file
 wget --no-verbose \
 	http://download.geofabrik.de/europe/united-kingdom-latest.osm.pbf \
---
+++ scripts/software_versions.sh
@@ -0,0 +1,7 @@
+echo "Debian $(cat /etc/debian_version)"
+python --version
+echo "Node $(node --version)"
+psql --version
+redis-server --version
+go version
+R --version | head -n 1
