deb9bfbe75d3152932e1a264116be3588a0cbb92
Greg <gregrs@fastmail.co.uk>
2026-06-04 20:07:19 +0100

    Add PostGIS to software version check

--- scripts/software_versions.sh
+++ scripts/software_versions.sh
@@ -2,6 +2,7 @@ echo "Debian $(cat /etc/debian_version)"
 python --version
 echo "Node $(node --version)"
 psql --version
+echo "PostGIS $(psql -At -d gregrs_fhrs -c 'SELECT PostGIS_Version();')"
 redis-server --version
 go version
 R --version | head -n 1
